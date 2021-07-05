9bf22b862a2d8f5906bf29f9a0e50fc76a993f09
Bartosz Piech <piech.bartosz696@gmail.com>
2021-07-05 13:27:53 +0200

    added replace function

--- bstringtest.c
+++ bstringtest.c
@@ -2,9 +2,10 @@
 #include "bstring.h"
 
 int main() {
-	bstring_t *my_string = bstring_new("hello world", 11);
+	bstring_t *my_string = bstring_new("raz dwa trzy dwa", 17);
 	bstring_print(my_string);
-	bstring_slice(my_string, 2, 8);
+	bstring_replace(my_string, "dwa", "siedem");
+	bstring_replace(my_string, "dwa", "siedem");
 	bstring_print(my_string);
 	bstring_delete(my_string);
 }
