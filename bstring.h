fdb328422cb7f710a5b632f2d87704e703c78d1c
Bartosz Piech <piech.bartosz696@gmail.com>
2021-07-05 07:27:37 +0200

    fixed copying strings and memory leak

--- bstring.h
+++ bstring.h
@@ -8,22 +8,24 @@ extern "C" {
 typedef struct {
 	char *data;
 	int len;
-} bstring_t, *bstring;
+} bstring_t;
 
 /* init -- destroy */
 /* new -- delete */
-int bstring_init(bstring str, const char *text);
-bstring bstring_new(const char *text);
-void bstring_destroy(bstring str);
-void bstring_delete(bstring str);
 
-bstring bstring_copy(bstring str);
-bstring bstring_cat(bstring str1, bstring str2);
-bstring bstring_mul(bstring str, int times);
+int bstring_init(bstring_t *str, const char *text, const int len);
+bstring_t *bstring_new(const char *text, const int len);
+void bstring_destroy(bstring_t *str);
+void bstring_delete(bstring_t *str);
 
-int bstring_find(bstring str, const char *text);
-int bstring_count(bstring str, const char ch);
+bstring_t *bstring_copy(bstring_t *str);
+bstring_t *bstring_cat(bstring_t *str1, bstring_t *str2);
+bstring_t *bstring_mul(bstring_t *str, const int times);
 
+int bstring_find(const bstring_t *str, const char *text);
+int bstring_count(const bstring_t *str, const char ch);
+
+void bstring_print(const bstring_t *str);
 
 #ifdef __cplusplus
 }
--- bstringtest.c
+++ bstringtest.c
@@ -3,35 +3,10 @@
 #include <math.h>
 #include "bstring.h"
 
-void print_f(float a) {
-	printf("%f\n", a);
-}
-void print_i(int a) {
-	printf("%d\n", a);
-}
-void print_s(char *a) {
-	printf("%s\n", a);
-}
-
-#define print(A)		\
-	_Generic ((A),		\
-		double: print_f,	\
-		int: print_i,	\
-		char *: print_s)(A)\
-
-#define fabs(X) \
-	_Generic((X), \
-			float: fabsf, \
-			long double: fabsl, \
-			default: fabs)(X)
-
-void bstring_print(bstring str) {
-	printf("%.*s\n", str->len, str->data);
-}
-
 int main() {
-	bstring my_string = bstring_new("MUO-");
-	bstring_mul(my_string, 5);
+	bstring_t *my_string = bstring_new("MUO-", 4);
+	bstring_mul(my_string, 50);
+	printf("%d, %.*s\n", my_string->len, my_string->len, my_string->data);
 	bstring_print(my_string);
 	bstring_delete(my_string);
 }
